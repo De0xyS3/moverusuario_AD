918c800982ffdbdd99ee504ce2c4b28332c05b8b
panconpalta <87393512+De0xyS3@users.noreply.github.com>
2024-07-28 14:51:01 -0500

    Agregar archivos de proyecto.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnLoadCSV = New Button()
+        btnSearch = New Button()
+        Label1 = New Label()
+        DataGridView1 = New DataGridView()
+        checkBoxFirstName = New CheckBox()
+        checkBoxLastName = New CheckBox()
+        checkBoxDisplayName = New CheckBox()
+        comboBoxOU = New ComboBox()
+        OpenFileDialog1 = New OpenFileDialog()
+        btnMoveAndDisable = New Button()
+        txtDomainName = New TextBox()
+        GroupBox1 = New GroupBox()
+        progressBar1 = New ProgressBar()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' btnLoadCSV
+        ' 
+        btnLoadCSV.Location = New Point(463, 37)
+        btnLoadCSV.Name = "btnLoadCSV"
+        btnLoadCSV.Size = New Size(121, 23)
+        btnLoadCSV.TabIndex = 0
+        btnLoadCSV.Text = "Cargar CSV"
+        btnLoadCSV.UseVisualStyleBackColor = True
+        ' 
+        ' btnSearch
+        ' 
+        btnSearch.Location = New Point(430, 66)
+        btnSearch.Name = "btnSearch"
+        btnSearch.Size = New Size(154, 23)
+        btnSearch.TabIndex = 1
+        btnSearch.Text = "Buscar Coincidencias"
+        btnSearch.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(349, 41)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(108, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Cargar archivo CSV"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Dock = DockStyle.Fill
+        DataGridView1.Location = New Point(3, 19)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(624, 296)
+        DataGridView1.TabIndex = 3
+        ' 
+        ' checkBoxFirstName
+        ' 
+        checkBoxFirstName.AutoSize = True
+        checkBoxFirstName.Location = New Point(12, 45)
+        checkBoxFirstName.Name = "checkBoxFirstName"
+        checkBoxFirstName.Size = New Size(165, 19)
+        checkBoxFirstName.TabIndex = 4
+        checkBoxFirstName.Text = "Buscar por nombre de pila"
+        checkBoxFirstName.UseVisualStyleBackColor = True
+        ' 
+        ' checkBoxLastName
+        ' 
+        checkBoxLastName.AutoSize = True
+        checkBoxLastName.Location = New Point(12, 70)
+        checkBoxLastName.Name = "checkBoxLastName"
+        checkBoxLastName.Size = New Size(132, 19)
+        checkBoxLastName.TabIndex = 5
+        checkBoxLastName.Text = "Buscar por apellidos"
+        checkBoxLastName.UseVisualStyleBackColor = True
+        ' 
+        ' checkBoxDisplayName
+        ' 
+        checkBoxDisplayName.AutoSize = True
+        checkBoxDisplayName.Location = New Point(12, 96)
+        checkBoxDisplayName.Name = "checkBoxDisplayName"
+        checkBoxDisplayName.Size = New Size(197, 19)
+        checkBoxDisplayName.TabIndex = 6
+        checkBoxDisplayName.Text = "Buscar por nombre para mostrar"
+        checkBoxDisplayName.UseVisualStyleBackColor = True
+        ' 
+        ' comboBoxOU
+        ' 
+        comboBoxOU.FormattingEnabled = True
+        comboBoxOU.Location = New Point(249, 12)
+        comboBoxOU.Name = "comboBoxOU"
+        comboBoxOU.Size = New Size(208, 23)
+        comboBoxOU.TabIndex = 8
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' btnMoveAndDisable
+        ' 
+        btnMoveAndDisable.Location = New Point(430, 96)
+        btnMoveAndDisable.Name = "btnMoveAndDisable"
+        btnMoveAndDisable.Size = New Size(154, 23)
+        btnMoveAndDisable.TabIndex = 9
+        btnMoveAndDisable.Text = "Mover y Deshabilitar"
+        btnMoveAndDisable.UseVisualStyleBackColor = True
+        ' 
+        ' txtDomainName
+        ' 
+        txtDomainName.Location = New Point(12, 12)
+        txtDomainName.Name = "txtDomainName"
+        txtDomainName.PlaceholderText = "Ingrese el nombre del dominio"
+        txtDomainName.Size = New Size(221, 23)
+        txtDomainName.TabIndex = 10
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(DataGridView1)
+        GroupBox1.Location = New Point(3, 120)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(630, 318)
+        GroupBox1.TabIndex = 11
+        GroupBox1.TabStop = False
+        ' 
+        ' progressBar1
+        ' 
+        progressBar1.Location = New Point(6, 441)
+        progressBar1.Name = "progressBar1"
+        progressBar1.Size = New Size(624, 23)
+        progressBar1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(636, 471)
+        Controls.Add(progressBar1)
+        Controls.Add(GroupBox1)
+        Controls.Add(txtDomainName)
+        Controls.Add(btnMoveAndDisable)
+        Controls.Add(comboBoxOU)
+        Controls.Add(checkBoxDisplayName)
+        Controls.Add(checkBoxLastName)
+        Controls.Add(checkBoxFirstName)
+        Controls.Add(Label1)
+        Controls.Add(btnSearch)
+        Controls.Add(btnLoadCSV)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MdiChildrenMinimizedAnchorBottom = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "https://github.com/De0xyS3"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnLoadCSV As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents checkBoxFirstName As CheckBox
+    Friend WithEvents checkBoxLastName As CheckBox
+    Friend WithEvents checkBoxDisplayName As CheckBox
+    Friend WithEvents comboBoxOU As ComboBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents btnMoveAndDisable As Button
+    Friend WithEvents txtDomainName As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents progressBar1 As ProgressBar
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,208 @@
+﻿Imports System.Data
+Imports System.DirectoryServices
+Imports System.IO
+Imports System.Windows.Forms
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        comboBoxOU.Enabled = False
+        btnMoveAndDisable.Enabled = False
+
+        ' Configurar DataGridView para que ocupe todo el espacio disponible en el GroupBox
+        DataGridView1.Dock = DockStyle.Fill
+
+        ' Inicializar la barra de progreso
+        progressBar1.Visible = False
+        progressBar1.Minimum = 0
+        progressBar1.Step = 1
+    End Sub
+
+    Private Sub btnLoadCSV_Click(sender As Object, e As EventArgs) Handles btnLoadCSV.Click
+        OpenFileDialog1.Filter = "CSV files (*.csv)|*.csv"
+        OpenFileDialog1.Title = "Select a CSV file"
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            progressBar1.Visible = True
+            progressBar1.Value = 0
+
+            Dim lines As String() = File.ReadAllLines(OpenFileDialog1.FileName)
+            Dim dt As New DataTable()
+            Dim headers As String() = lines(0).Split(","c)
+
+            For Each header As String In headers
+                dt.Columns.Add(header)
+            Next
+
+            progressBar1.Maximum = lines.Length - 1
+
+            For i As Integer = 1 To lines.Length - 1
+                Dim rows As String() = lines(i).Split(","c)
+                Dim dr As DataRow = dt.NewRow()
+                For j As Integer = 0 To headers.Length - 1
+                    dr(j) = rows(j)
+                Next
+                dt.Rows.Add(dr)
+
+                ' Actualizar la barra de progreso
+                progressBar1.PerformStep()
+            Next
+
+            DataGridView1.DataSource = dt
+
+            ' Deshabilitar comboBoxOU y btnMoveAndDisable después de cargar un nuevo CSV
+            comboBoxOU.Enabled = False
+            btnMoveAndDisable.Enabled = False
+
+            ' Ocultar la barra de progreso
+            progressBar1.Visible = False
+        End If
+    End Sub
+
+    Private Sub checkBoxFirstName_CheckedChanged(sender As Object, e As EventArgs) Handles checkBoxFirstName.CheckedChanged
+        If checkBoxFirstName.Checked Then
+            checkBoxLastName.Checked = False
+            checkBoxDisplayName.Checked = False
+        End If
+    End Sub
+
+    Private Sub checkBoxLastName_CheckedChanged(sender As Object, e As EventArgs) Handles checkBoxLastName.CheckedChanged
+        If checkBoxLastName.Checked Then
+            checkBoxFirstName.Checked = False
+            checkBoxDisplayName.Checked = False
+        End If
+    End Sub
+
+    Private Sub checkBoxDisplayName_CheckedChanged(sender As Object, e As EventArgs) Handles checkBoxDisplayName.CheckedChanged
+        If checkBoxDisplayName.Checked Then
+            checkBoxFirstName.Checked = False
+            checkBoxLastName.Checked = False
+        End If
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim domainName As String = txtDomainName.Text.Trim()
+        If String.IsNullOrEmpty(domainName) Then
+            MessageBox.Show("Por favor, ingrese el nombre del dominio.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim searchResults As New DataTable()
+        searchResults.Columns.Add("FirstName")
+        searchResults.Columns.Add("LastName")
+        searchResults.Columns.Add("DisplayName")
+        searchResults.Columns.Add("ADPath")
+
+        For Each row As DataGridViewRow In DataGridView1.Rows
+            If row.IsNewRow Then Continue For
+
+            Dim searchString As String = BuildSearchFilter(row)
+            If String.IsNullOrEmpty(searchString) Then Continue For
+
+            Dim result As SearchResult = FindUserInAD(searchString, domainName)
+            If result IsNot Nothing Then
+                Dim dr As DataRow = searchResults.NewRow()
+                dr("FirstName") = row.Cells("FirstName").Value
+                dr("LastName") = row.Cells("LastName").Value
+                dr("DisplayName") = row.Cells("DisplayName").Value
+                dr("ADPath") = result.Path
+                searchResults.Rows.Add(dr)
+            End If
+        Next
+
+        DataGridView1.DataSource = searchResults
+        comboBoxOU.Enabled = True
+        btnMoveAndDisable.Enabled = True
+        LoadOrganizationalUnits(domainName)
+    End Sub
+
+    Private Sub btnMoveAndDisable_Click(sender As Object, e As EventArgs) Handles btnMoveAndDisable.Click
+        Dim newOU As String = comboBoxOU.SelectedItem.ToString()
+        Dim domainName As String = txtDomainName.Text.Trim()
+
+        progressBar1.Visible = True
+        progressBar1.Value = 0
+        progressBar1.Maximum = DataGridView1.Rows.Count - 1
+
+        For Each row As DataGridViewRow In DataGridView1.Rows
+            If row.IsNewRow Then Continue For
+
+            Dim adPath As String = row.Cells("ADPath").Value.ToString()
+            Dim entry As New DirectoryEntry(adPath)
+
+            Try
+                MoveUserToOU(entry, newOU)
+                DisableADUser(entry)
+                MessageBox.Show($"Usuario {entry.Name} movido y deshabilitado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show($"Error al mover o deshabilitar al usuario {entry.Name}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+            ' Actualizar la barra de progreso
+            progressBar1.PerformStep()
+        Next
+
+        ' Ocultar la barra de progreso
+        progressBar1.Visible = False
+    End Sub
+
+    Private Sub LoadOrganizationalUnits(domainName As String)
+        Dim ouList As List(Of String) = GetOrganizationalUnits(domainName)
+        comboBoxOU.DataSource = ouList
+    End Sub
+
+    Private Function GetOrganizationalUnits(domainName As String) As List(Of String)
+        Dim ouList As New List(Of String)()
+        Dim entry As New DirectoryEntry($"LDAP://DC={domainName.Replace(".", ",DC=")}")
+        Dim searcher As New DirectorySearcher(entry)
+
+        searcher.Filter = "(objectCategory=organizationalUnit)"
+        searcher.SearchScope = SearchScope.Subtree
+        searcher.PropertiesToLoad.Add("distinguishedName")
+
+        For Each result As SearchResult In searcher.FindAll()
+            If result.Properties.Contains("distinguishedName") Then
+                ouList.Add(result.Properties("distinguishedName")(0).ToString())
+            End If
+        Next
+
+        Return ouList
+    End Function
+
+    Private Function BuildSearchFilter(row As DataGridViewRow) As String
+        Dim filter As String = String.Empty
+
+        If checkBoxFirstName.Checked Then
+            filter &= $"(givenName={row.Cells("FirstName").Value})"
+        End If
+        If checkBoxLastName.Checked Then
+            filter &= $"(sn={row.Cells("LastName").Value})"
+        End If
+        If checkBoxDisplayName.Checked Then
+            filter &= $"(displayName={row.Cells("DisplayName").Value})"
+        End If
+
+        If Not String.IsNullOrEmpty(filter) Then
+            filter = $"(|{filter})"
+        End If
+
+        Return filter
+    End Function
+
+    Private Function FindUserInAD(searchString As String, domainName As String) As SearchResult
+        Dim entry As New DirectoryEntry($"LDAP://DC={domainName.Replace(".", ",DC=")}")
+        Dim search As New DirectorySearcher(entry)
+        search.Filter = searchString
+        search.PropertiesToLoad.Add("cn")
+        Return search.FindOne()
+    End Function
+
+    Private Sub MoveUserToOU(entry As DirectoryEntry, newOU As String)
+        Dim newPath As String = $"LDAP://{newOU}"
+        entry.MoveTo(New DirectoryEntry(newPath))
+    End Sub
+
+    Private Sub DisableADUser(entry As DirectoryEntry)
+        entry.Properties("userAccountControl").Value = &H202 ' Disable account
+        entry.CommitChanges()
+    End Sub
+End Class
